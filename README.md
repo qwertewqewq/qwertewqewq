e43024f1f3f3b335902b975827a404669ada5add
qwertewqewq <rasdwe231r@gmail.com>
2024-10-17 11:02:09 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# Math-2024-25-Winter
+
+### Konfiguracja środowiska pracy
+
+1. Założenie konta na [GitHub](http://github.com) (darmowe!) -- warunek konieczny do pracy oraz zaliczenia zajęć.
+   
+2. Konto na [Google Colab](https://colab.research.google.com/) lub lokalne środowisko Pythona (np. Anaconda) -- warunek konieczny do pracy oraz zaliczenia zajęć.
+   
+3. Wykorzystywane technologie: 
+   * Python [https://www.python.org/](https://www.python.org/)
+   * Visual Studio Code [https://code.visualstudio.com/](https://code.visualstudio.com/)
+      * **Plugins**: Python, GitHub Copilot, GitHub Repositories, Remote Repositories, Jupyter, Git Graph
+   * Markdown [https://www.markdownguide.org/](https://www.markdownguide.org/) oraz [link](https://quarto.org/docs/authoring/markdown-basics.html)
+   * Jupyter Notebook [https://jupyter.org/](https://jupyter.org/)
+   * Google Colab [https://colab.research.google.com/](https://colab.research.google.com/)
+   * GitHub: [http://github.com](http://github.com)
+   * LaTeX: [https://www.latex-project.org/get/](https://www.latex-project.org/get/) oraz [link](https://tilburgsciencehub.com/topics/research-skills/templates-dynamic-content/templates/amsmath-latex-cheatsheet/)
+   * Geogebra [https://www.geogebra.org/](https://www.geogebra.org/)
+
+
+# Math-2024-25-Winter
+
+### Working Environment Configuration
+
+1. Create an account on [GitHub](http://github.com) (free!) -- required for participation and course completion.
+   
+2. An account on [Google Colab](https://colab.research.google.com/) or a local Python environment (e.g., Anaconda) -- required for participation and course completion.
+   
+3. Technologies used: 
+   * Python [https://www.python.org/](https://www.python.org/)
+   * Visual Studio Code [https://code.visualstudio.com/](https://code.visualstudio.com/)
+      * **Plugins**: Python, GitHub Copilot, GitHub Repositories, Remote Repositories, Jupyter, Git Graph
+   * Markdown [https://www.markdownguide.org/](https://www.markdownguide.org/) and [link](https://quarto.org/docs/authoring/markdown-basics.html)
+   * Jupyter Notebook [https://jupyter.org/](https://jupyter.org/)
+   * Google Colab [https://colab.research.google.com/](https://colab.research.google.com/)
+   * GitHub: [http://github.com](http://github.com)
+   * LaTeX: [https://www.latex-project.org/get/](https://www.latex-project.org/get/) and [link](https://tilburgsciencehub.com/topics/research-skills/templates-dynamic-content/templates/amsmath-latex-cheatsheet/)
+   * Geogebra [https://www.geogebra.org/](https://www.geogebra.org/)
+
+---
---
+++ setup.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# Funkcja tworząca katalog i plik README.md z tytułem
+create_dir_with_readme() {
+    dirname="$1"
+    mkdir -p "$dirname"
+    echo "# $dirname" > "$dirname/readme.md"
+}
+
+# Tworzenie katalogów na notatniki
+create_dir_with_readme Notebooks_PL
+create_dir_with_readme Notebooks_EN
+
+# Przejście do katalogu Notebooks_PL
+cd Notebooks_PL
+
+# Tworzenie podkatalogów dla wersji polskiej
+create_dir_with_readme 00_Wstep
+create_dir_with_readme 01_Algebra_Liniowa
+create_dir_with_readme 02_Geometria_Analityczna
+create_dir_with_readme 03_Analiza_Matematyczna
+
+############################################
+# Algebra Liniowa
+cd 01_Algebra_Liniowa
+
+create_dir_with_readme 01_Macierze
+create_dir_with_readme 02_Uklady_Rownan_Liniowych
+create_dir_with_readme 03_Wektory_definicje
+create_dir_with_readme 04_Wektory_dzialania
+
+cd ..
+
+############################################
+# Geometria Analityczna
+cd 02_Geometria_Analityczna
+
+create_dir_with_readme 05_Proste
+create_dir_with_readme 06_Krzywe_Stozkowe
+create_dir_with_readme 07_Plaszczyzny_W_Przestrzeni
+create_dir_with_readme 08_Powierzchnie_Drugiego_Rzedu
+create_dir_with_readme 09_Podstawy_Rachunku_Tensorowego
+
+cd ..
+
+############################################
+# Analiza Matematyczna
+cd 03_Analiza_Matematyczna
+
+create_dir_with_readme 10_Ciagi
+create_dir_with_readme 11_Funkcje
+create_dir_with_readme 12_Pochodna_Funkcji
+create_dir_with_readme 13_Rozniczka
+create_dir_with_readme 14_Zastosowania_Rachunku_Rozniczkowego
+create_dir_with_readme 15_Calka_Nieoznaczona
+create_dir_with_readme 16_Metody_calkowania_I
+create_dir_with_readme 17_Metody_calkowania_II
+create_dir_with_readme 18_Calka_Oznaczona
+create_dir_with_readme 19_Calki_Niewlasciwe
+create_dir_with_readme 20_Zastosowania_Calek_Oznaczonych
+create_dir_with_readme 21_Szeregi_Liczbowe
+create_dir_with_readme 22_Zbieznosc_Szeregow
+create_dir_with_readme 23_Szeregi_Funkcyjne
+create_dir_with_readme 24_Szeregi_Taylora_i_Maclaurina
+create_dir_with_readme 25_Szeregi_Fouriera
+
+cd ../..
+
+############################################
+# Przejście do katalogu Notebooks_EN
+cd Notebooks_EN
+
+# Tworzenie podkatalogów dla wersji angielskiej
+create_dir_with_readme 00_Introduction
+create_dir_with_readme 01_Linear_Algebra
+create_dir_with_readme 02_Analytic_Geometry
+create_dir_with_readme 03_Mathematical_Analysis
+
+############################################
+# Linear Algebra
+cd 01_Linear_Algebra
+
+create_dir_with_readme 01_Matrices
+create_dir_with_readme 02_Systems_of_Linear_Equations
+create_dir_with_readme 03_Vectors_definitions
+create_dir_with_readme 04_Vector_operations
+
+cd ..
+
+############################################
+# Analytical Geometry
+cd 02_Analytic_Geometry
+
+create_dir_with_readme 05_Lines
+create_dir_with_readme 06_Conic_Curves
+create_dir_with_readme 07_Planes_in_Space
+create_dir_with_readme 08_Second_Order_Surfaces
+create_dir_with_readme 09_Basics_of_Tensor_Calculus
+
+cd ..
+
+############################################
+# Mathematical Analysis
+cd 03_Mathematical_Analysis
+
+create_dir_with_readme 10_Sequences
+create_dir_with_readme 11_Functions
+create_dir_with_readme 12_Derivatives
+create_dir_with_readme 13_Differential
+create_dir_with_readme 14_Applications_of_Differential_Calculus
+create_dir_with_readme 15_Indefinite_Integral
+create_dir_with_readme 16_Integration_Methods_I
+create_dir_with_readme 17_Integration_Methods_II
+create_dir_with_readme 18_Definite_Integral
+create_dir_with_readme 19_Improper_Integrals
+create_dir_with_readme 20_Applications_of_Integral_Calculus
+create_dir_with_readme 21_Numerical_Series
+create_dir_with_readme 22_Series_Convergence
+create_dir_with_readme 23_Functional_Series
+create_dir_with_readme 24_Taylor_and_Maclaurin_Series
+create_dir_with_readme 25_Fourier_Series
+
+cd ../..
+
+# Wyświetlenie listy katalogów i plików
+ls -la
